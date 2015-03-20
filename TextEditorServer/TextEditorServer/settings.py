cd9e799513e57be2a5f217cc77fea84d19a16e7c
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-03-21 00:47:44 +0100

    Jenkins Test no 2.

--- TextEditorServer/TextEditorServer/settings.py
+++ TextEditorServer/TextEditorServer/settings.py
@@ -43,7 +43,6 @@ JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
     'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.run_jslint',
     'django_jenkins.tasks.run_csslint',
     'django_jenkins.tasks.run_sloccount'
 )
