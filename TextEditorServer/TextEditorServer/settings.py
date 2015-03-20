c5ff9f28d9df9b78614e9c50a5a06adf88487279
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-03-20 18:44:29 +0100

    Added jenkins plugin to server.

--- TextEditorServer/TextEditorServer/settings.py
+++ TextEditorServer/TextEditorServer/settings.py
@@ -36,6 +36,14 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
+)
+
+JENKINS_TASKS = (
+        'django_jenkins.tasks.with_coverage',
+        'django_jenkins.tasks.run_pep8',
+        'django_jenkins.tasks.run_pyflakes',
+        'django_jenkins.tasks.run_pylint',
 )
 
 MIDDLEWARE_CLASSES = (
