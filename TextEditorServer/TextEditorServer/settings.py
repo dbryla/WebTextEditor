12ee728d7584e44c3ee6cf1187e858cefc030ca8
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-03-21 01:20:19 +0100

    Jenkins Test no 5.

--- TextEditorServer/TextEditorServer/settings.py
+++ TextEditorServer/TextEditorServer/settings.py
@@ -42,9 +42,7 @@ INSTALLED_APPS = (
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.run_csslint',
-    'django_jenkins.tasks.run_sloccount'
+    'django_jenkins.tasks.run_pylint'
 )
 
 PROJECT_APPS = (
